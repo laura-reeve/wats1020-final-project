ec86b0949d7523e2715e9d679cb5f3097c32555d
lauralee51 <lauralee51@gmail.com>
2017-05-20 21:45:25 -0400

    fixed game resizing?

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,13 @@
 // My original JS styles will go here.
 
 $(document).ready( function() {
-  console.log("main.js works");
-});
+  
+    // resize flash player according to viewport height/width
+  $(window).on('resize orientationChange', function(event) {
+    var width = $(window).width();
+    var height = $(window).height();
+    $.resize(width);
+    $(".miniclip-game-embed").resize(width, height);
+  });
+  
+});  // ready
