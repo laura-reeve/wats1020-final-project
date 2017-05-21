c503a147a1bd75a86a83c285cfb8b539236bb7a5
lauralee51 <lauralee51@gmail.com>
2017-05-21 15:41:26 -0400

    assignment complete

--- JS/main.js
+++ JS/main.js
@@ -2,11 +2,20 @@
 
 $(document).ready( function() {
   
-    // resize flash player according to viewport height/width
-  $(window).on('resize orientationChange', function(event) {
-    var width = $(window).width();
-    var height = $(window).height();
-    $(".miniclip-game-embed").resize(width, height);
-  });
+  // Need to refresh browser at XS size for this to work.
+  // At XS size...
+function responsive() {
+  // Recommend mobile app (game does not resize below 500)
+  $("#flash-warning").html("<a href='#'>Mobile app</a> recommended at this size.");
+  // Hide ad space at top 
+  $("footer").html("<h4 style='text-align:center;padding-top:20px;padding-bottom:20px;'>Ad space</h4>");
+  // Change text at bottom to accommodate ad space
+  $("#ad-space").hide();
+}
+  // Run function at 500px
+  var width = $(window).width();
+  if (width < 500) {
+    responsive();
+  }
   
 });  // ready
